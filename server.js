b351ff53cec401cfb47a061e2c06a60c2cf93353
karolinneczka <okaa93@gmail.com>
2018-07-12 21:24:26 +0200

    Add writefile inside readifile - not working

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var fs = require('fs'); //wbudowany w node - nie trzeba instalować
 
 //stworzenie aplikacji Express:
 var app = express();
-var stringifyFile = data;
+var stringifyFile;
 app.use(bodyParser.json()); //pozwoli wykorzystać middleware body-parser
 
 app.get('/', function (req, res) {
@@ -22,16 +22,28 @@ app.get('/getNote', function (req, res) {
     console.log('Otrzymałem żądanie GET do strony /getNote');
     fs.readFile('./test.json', 'utf8', function(err, data) {
         if (err) throw err;
-        // stringifyFile = data;
+        stringifyFile = data;
         res.send(data);
     });
 });
 
+// app.post('/updateNote/:note', function (req, res) {
+//     stringifyFile = stringifyFile + req.params.note;
+//     fs.writeFile('./test.json', stringifyFile, function(err) {
+//         if (err) throw err;
+//         console.log('file updated');
+//     });
+// });
+
 app.post('/updateNote/:note', function (req, res) {
-    stringifyFile = stringifyFile + req.params.note;
-    fs.writeFile('./test.json', stringifyFile, function(err) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
         if (err) throw err;
-        console.log('file updated');
+        stringifyFile = data + req.params.note;
+        fs.writeFile('./test.json', stringifyFile, function(err) {
+            if (err) throw err;
+            console.log('file updated');
+        });
+        res.send(data);
     });
 });
 
